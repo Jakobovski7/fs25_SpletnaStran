310f6145cb66e2f46210758f9274e894c7839544
Jakobovski7 <jakob.public@gmail.com>
2026-03-26 14:48:14 +0100

    Dodana skripta za datum zadnje spremembe

--- Teme.js
+++ Teme.js
@@ -19,6 +19,28 @@ document.addEventListener("DOMContentLoaded", () => {
     hamburger.addEventListener("click", () => {
         sidebar.classList.toggle("open");
     });
+
+    // Ta skripta izpiše datum zadnje spremembe //
+
+    const outputElement = document.getElementById("lastModified"); 
+    const lastModified = document.lastModified;
+    
+    const deli = lastModified.split(" ");  //razcleni datum iz osnovne oblike (dd/mm/yyy hh/mm/ss), vmes je presledek, po katerem se lahko to loci.
+    const datum = deli[0];  //naredi konstanto datum
+    const cas = deli[1];    //naredi konstanto cas
+
+    const danDel = datum.split("/");    //razdelimo konstanto datum ob /
+    const mesec = danDel[0];    //naredimo konstanto mesec
+    const dan = danDel[1];  //naredimo konstanto dan 
+    const leto = danDel[2]; //naredrmo konstanto leto
+
+    const casDel = cas.split(":");  //razdelimo konsranto cas ob :
+    const ura = casDel[0];  //naredimo konstanto ura
+    const minuta = casDel[1];   //naredimo konstanto minuta
+    const sekunda = casDel[2];  //naredimo konstanto sekunda
+
+    const skupaj = `Zadnja sprememba strani: ${dan}. ${mesec}. ${leto} ob ${ura}:${minuta}:${sekunda}`; //vse konstante sestavimo skupaj v koncno obliko zapisa
+    outputElement.textContent = skupaj; //izpisemo koncno obliko
 })
 
 
